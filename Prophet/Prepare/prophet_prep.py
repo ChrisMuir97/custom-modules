97590ec37e815cc951d954a9b6bab0e07e2fa74c
chris-muir <46712312+chris-muir@users.noreply.github.com>
2021-05-18 11:04:03 +0100

    Add files via upload

---
+++ Prophet/Prepare/prophet_prep.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import argparse
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from azureml.studio.core.io.data_frame_directory import load_data_frame_from_directory, save_data_frame_to_directory
+
+## Parse args
+parser = argparse.ArgumentParser("Prophet-Prep")
+parser.add_argument("--Loaded_Data", type=str, help="Loaded dataset")
+parser.add_argument("--Timeseries", type=str, help="Timeseries column.")
+parser.add_argument("--Forecast", type=str, help="Forecast column")
+parser.add_argument("--Prepared_Data", type=str, help="Prepared dataset")
+args = parser.parse_args()
+
+## Load data from DataFrameDirectory to Pandas DataFrame
+training_df = load_data_frame_from_directory(args.Loaded_Data).data
+
+## Prepare training data
+training_df = training_df[[args.Timeseries , args.Forecast]].copy()
+training_df = training_df.rename(columns = {args.Timeseries:'ds', args.Forecast:'y'})
+training_df['ds'] = pd.to_datetime(training_df['ds'])
+
+## Output prepared Pandas DataFrame to DataFrameDirectory
+save_data_frame_to_directory(args.Prepared_Data, training_df)
